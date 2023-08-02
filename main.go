6266ab3cc3e3bf34226e079306b031a3ff952491
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2023-08-02 02:37:59 -0300

    add function code

--- main.go
+++ main.go
@@ -1,10 +1,52 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler() error {
+type TickerEvent struct {
+	Frequency string `json:"frequency"`
+}
+
+type PeriodicWorkData struct {
+	Frequency string `json:"frequency"`
+}
+
+func handler(event TickerEvent) error {
+	url := fmt.Sprintf("https://%s/admin/periodic-work", os.Getenv("ENGINE_HOSTNAME"))
+	data := PeriodicWorkData(event)
+	dataJson, err := json.Marshal(data)
+
+	if err != nil {
+		return err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(dataJson))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %v", os.Getenv("ENGINE_SECRET")))
+
+	if err != nil {
+		return err
+	}
+
+	response, nil := http.DefaultClient.Do(request)
+
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return fmt.Errorf("engine: %d", response.StatusCode)
+	}
+
 	return nil
 }
 
